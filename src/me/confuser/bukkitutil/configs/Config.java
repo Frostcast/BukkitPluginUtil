f6211e7599d173e9ec25ec521425c6fccbf73d22
James Mortemore <jamesmortemore@gmail.com>
2015-04-11 16:59:46 +0100

    More robust config loading

--- src/me/confuser/bukkitutil/configs/Config.java
+++ src/me/confuser/bukkitutil/configs/Config.java
@@ -1,153 +1,170 @@
 package me.confuser.bukkitutil.configs;
 
+import me.confuser.bukkitutil.BukkitPlugin;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
-
-import me.confuser.bukkitutil.BukkitPlugin;
-
-import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.Plugin;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Inspired by md_5
- *
+ * <p/>
  * An awesome super-duper-lazy Config lib! Just extend it, set some (non-static)
  * variables
- *
+ * <p/>
  * Originally by:
  *
  * @author codename_B
- * @version 2.2
- *
- *          Modified by:
  * @author confuser
- *
+ * @version 2.2
+ *          <p/>
+ *          Modified by:
  */
-public abstract class Config <T extends BukkitPlugin> {
-	private Class<T> clazz;
-	protected T plugin;
+public abstract class Config<T extends BukkitPlugin> {
 
-	protected File file = null;
-	public YamlConfiguration conf = new YamlConfiguration();
+  private Class<T> clazz;
+  protected T plugin;
 
-	@SuppressWarnings("unchecked")
-	public Config(Object file) {
-		clazz = ((Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0]);
+  protected File file = null;
+  public YamlConfiguration conf = new YamlConfiguration();
 
-		Method method = null;
-		try {
-			method = clazz.getDeclaredMethod("getPlugin", null);
-		} catch (NoSuchMethodException | SecurityException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		try {
-			plugin = (T) method.invoke(null, method.getParameterTypes());
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		setFile(file);
-	}
+  @SuppressWarnings("unchecked")
+  public Config(Object file) {
+    clazz = ((Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0]);
 
-	/**
-	 * Must be called before using config.load() or config.save();
-	 *
-	 * @param input
-	 * @return (Config) instance
-	 */
-	public Config<T> setFile(Object input) {
-		// handle the File
-		if (input == null) {
-			new InvalidConfigurationException("File cannot be null!").printStackTrace();
-		} else if (input instanceof File) {
-			// the file, directly
-			file = (File) input;
-		} else if (input instanceof Plugin) {
-			// the config.yml of the plugin
-			file = getFile((Plugin) input);
-		} else if (input instanceof String) {
-			// the literal file from the string
-			file = new File(plugin.getDataFolder(), (String) input);
-		}
-		return this;
-	}
+    Method method = null;
+    try {
+      method = clazz.getDeclaredMethod("getPlugin", null);
+    } catch (NoSuchMethodException | SecurityException e1) {
+      // TODO Auto-generated catch block
+      e1.printStackTrace();
+    }
+    try {
+      plugin = (T) method.invoke(null, method.getParameterTypes());
+    } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
 
-	/**
-	 * Lazy load
-	 */
-	public void load() {
-		if (file != null) {
-			try {
-				onLoad(file);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-			new InvalidConfigurationException("File cannot be null!").printStackTrace();
-		}
+    setFile(file);
+  }
 
-		afterLoad();
-	}
+  /**
+   * Must be called before using config.load() or config.save();
+   *
+   * @param input
+   *
+   * @return (Config) instance
+   */
+  public Config<T> setFile(Object input) {
+    // handle the File
+    if (input == null) {
+      new InvalidConfigurationException("File cannot be null!").printStackTrace();
+    } else if (input instanceof File) {
+      // the file, directly
+      file = (File) input;
+    } else if (input instanceof Plugin) {
+      // the config.yml of the plugin
+      file = getFile((Plugin) input);
+    } else if (input instanceof String) {
+      // the literal file from the string
+      file = new File(plugin.getDataFolder(), (String) input);
+    }
+    return this;
+  }
 
-	public abstract void afterLoad();
+  /**
+   * Lazy load
+   */
+  public void load() {
+    if (file != null) {
+      try {
+        onLoad(file);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    } else {
+      new InvalidConfigurationException("File cannot be null!").printStackTrace();
+    }
 
-	public void setDefaults() {
-		plugin.saveResource(file.getName(), false);
-	};
+    afterLoad();
+  }
 
-	/**
-	 * Lazy save
-	 *
-	 * @throws Exception
-	 */
-	public abstract void onSave();
+  public abstract void afterLoad();
 
-	public void onLoad(File file) throws Exception {
-		if (!file.exists()) {
-			if (file.getParentFile() != null)
-				file.getParentFile().mkdirs();
+  public void setDefaults() {
+    plugin.saveResource(file.getName(), false);
+  }
 
-			// Set the defaults first
-			setDefaults();
-		}
+  ;
 
-		conf.load(file);
+  /**
+   * Lazy save
+   *
+   * @throws Exception
+   */
+  public abstract void onSave();
 
-		// Look for defaults in the jar
-		InputStream defConfigStream = plugin.getResource(file.getName());
-		if (defConfigStream != null) {
-			YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
-			conf.setDefaults(defConfig);
-			conf.options().copyDefaults(true);
-		}
+  public void onLoad(File file) throws Exception {
+    if (!file.exists()) {
+      if (file.getParentFile() != null)
+        file.getParentFile().mkdirs();
 
-		save();
-	}
+      // Set the defaults first
+      setDefaults();
+    }
 
-	public void save() {
-		onSave();
+    try {
+      conf.load(file);
+    } catch (InvalidConfigurationException e) {
+      plugin.getLogger().severe("Invalid yaml file " + file.getName());
+      e.printStackTrace();
 
-		try {
-			conf.save(file);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+      plugin.getLogger().severe("Attempting to load default " + file.getName());
 
-	}
+      file.renameTo(new File(plugin.getDataFolder(), file.getName().replace(".yml", "") + new SimpleDateFormat
+              ("yyyy-MM-dd_HH-mm-ss").format(new Date()) + ".yml.invalid"));
+      plugin.saveResource(file.getName(), false);
+
+      onLoad(file);
+    }
+
+    // Look for defaults in the jar
+    InputStream defConfigStream = plugin.getResource(file.getName());
+    if (defConfigStream != null) {
+      YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+      conf.setDefaults(defConfig);
+      conf.options().copyDefaults(true);
+    }
+
+    save();
+  }
+
+  public void save() {
+    onSave();
+
+    try {
+      conf.save(file);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
 
 	/*
-	 * Utility methods
+   * Utility methods
 	 */
 
-	private File getFile(Plugin plugin) {
-		return new File(plugin.getDataFolder(), "config.yml");
-	}
+  private File getFile(Plugin plugin) {
+    return new File(plugin.getDataFolder(), "config.yml");
+  }
 
 }
